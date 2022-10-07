1ec58c1a15d23513070086355d2c5fb93f3ec1e1
sdong4884 <sdong4884@gmail.com>
2022-10-07 12:54:39 +0900

    TodoCreate 기능 구현하기

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { MdAdd } from 'react-icons/md';
+import { useTodoDispatch, useTodoNextId } from "../TodoContext";
 
 const CircleButton = styled.button`
   background: #38d9a9;
@@ -45,7 +46,7 @@ const InsertFormPositioner = styled.div`
   bottom: 0;
   left: 0;
 `
-const InsertForm = styled.div`
+const InsertForm = styled.form`
   background: #f8f9fa;
   padding: 32px 32px 72px;
   border-bottom-left-radius: 16px;
@@ -65,16 +66,38 @@ const Input = styled.input`
 
 function TodoCreate () {
   const [open, setOpen] = useState(false)
+  const [value, setValue] = useState('')
+
   const onToggle = () => setOpen(!open)
+  const onChange = (e) => setValue(e.target.value)
+  const onSubmit = (e) => {
+    e.preventDefault()
+    dispatch({
+      type: 'CREATE',
+      todo: {
+        id: nextId.current,
+        text: value,
+        done: false
+      }
+    })
+    setValue('')
+    setOpen(false)
+    nextId.current += 1
+  }
+
+  const dispatch = useTodoDispatch()
+  const nextId = useTodoNextId()
 
   return (
     <>
       {open && (
         <InsertFormPositioner>
-          <InsertForm>
+          <InsertForm onSubmit={onSubmit}>
             <Input 
               placeholder="할 일을 입력 후, Enter를 누르세요" 
               autoFocus
+              onChange={onChange}
+              value={value}
             />
           </InsertForm>
         </InsertFormPositioner>
@@ -89,4 +112,4 @@ function TodoCreate () {
   )
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
